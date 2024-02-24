8de7aca97ff461041962e13380a15686fbd23a27
MSChella <reachchellapathy.ms@gmail.com>
2024-02-24 14:22:38 +0530

    Env variable work around

--- dbConfig.js
+++ dbConfig.js
@@ -5,7 +5,7 @@ async function connectDatabase() {
     const uri =
         process.env.NODE_ENV === "development"
             ? `${process.env.MONGODB_DEV_URI}/${process.env.MONGODB_DEV_NAME}`
-            : `${process.env.MONGODB_PROD_URI}ordersdb?retryWrites=true&w=majority&appName=Cluster0`;
+            : `${process.env.MONGODB_PROD_URI}${process.env.MONGODB_PROD_NAME}`;
 
 
     try {
