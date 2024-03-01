e45b67d2d29efda8ba864f7f527afd50384f5dda
MSChella <reachchellapathy.ms@gmail.com>
2024-03-01 10:42:07 +0530

    Hardcoded need to remove later

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,16 +4,14 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const token = req.header({
-    "Authorization": "Bearer YOUR_TOKEN_HERE"
-  });
+  const token = req.header('Authorization');
   console.log('Token:', token);
   if (!token) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, ' process.env.JWT_SECRET');
     req.user = decoded;
     next();
   } catch (error) {
