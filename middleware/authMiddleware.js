ccd0dd3de6d1b93a7bb3c84b93e175fc3d14c577
MSChella <reachchellapathy.ms@gmail.com>
2024-03-01 09:57:28 +0530

    debug

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 // authMiddleware.js
+
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
@@ -9,7 +11,7 @@ const verifyToken = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, 'mySecretKey123');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
