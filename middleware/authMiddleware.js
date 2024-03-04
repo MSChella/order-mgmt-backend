d1938ecbee6b5cec70e7d4889d3a038e337a05a9
MSChella <reachchellapathy.ms@gmail.com>
2024-03-04 15:47:05 +0530

    Debugging for auth routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  console.log('Token:', token);
+  const authHeader = req.header('Authorization');
+  // const token = req.header('Authorization');
+  console.log('Auth Header:', authHeader);
+  // console.log('Token:', token);
   if (!token) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
-
+  const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, 'mySecretKey123');
     req.user = decoded;
