8fce7d6c05862fdcf3b9bb3f85617e9f87f7b43a
MSChella <reachchellapathy.ms@gmail.com>
2024-03-04 11:25:17 +0530

    env variables

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,7 +11,7 @@ const verifyToken = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, 'mySecretKey123');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
