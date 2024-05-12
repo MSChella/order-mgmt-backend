6d9be366f8b0d5604bebec676a79cfe654893e02
MSChella <reachchellapathy.ms@gmail.com>
2024-05-12 18:24:11 +0530

    CORS url updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://bookmybook.netlify.app'],
+    origin: ['http://localhost:3000', 'https://bookmybook.netlify.app'],
     optionsSuccessStatus: 200,
     allowedHeaders: ['Authorization', 'Content-Type'],
 
